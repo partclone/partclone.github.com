8d00e503151d86c8f274b3a42f300946be5fd227
Thomas Tsai <thomas@nchc.org.tw>
2011-01-18 09:57:07 +0800

    add donate.php to git

---
+++ donate.php
@@ -0,0 +1,65 @@
+<?php include("header.php");?>
+
+<div class="panel" align="justify"> <span class="orangetitle">Donation</span>
+<span class="bodytext">
+<br /><strong>Donattion</strong>
+If you use Partclone and it does the job for you, please donate to help us to make Partclone better.
+
+<!--<li><form id="donate_paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" target=_blank> <p>Donate via PayPal: <input name="amount" value="2" checked="checked" type="radio"> $2 <input name="amount" value="5" type="radio"> $5 <input name="amount" value="10" type="radio"> $10 <input name="amount" value="20" type="radio"> $20 <input name="amount" value="" type="radio"> Other <input name="submit" class="button" value="Donate!" type="submit"> <input name="cmd" value="_xclick" type="hidden"> <input name="business" value="thomas@nchc.org.tw" type="hidden"> <input name="item_name" value="Clonezilla Project" type="hidden"> <input name="no_shipping" value="1" type="hidden"> </p></form></li>-->
+
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+Donate via PayPal: <input name="amount" value="2" checked="checked" type="radio"> $2 <input name="amount" value="5" type="radio"> $5 <input name="amount" value="10" type="radio"> $10 <input name="amount" value="20" type="radio"> $20 <input name="amount" value="" type="radio"> Other
+<input type="hidden" name="cmd" value="_donations">
+<input type="hidden" name="business" value="thomas@nchc.org.tw">
+<input type="hidden" name="item_name" value="partclone.org">
+<input name="submit" class="button" value="Donate!" type="submit">
+</form>
+</span>
+</div>
+<div class="panel" align="justify"> <span class="orangetitle">Donate List</span>
+<br /><span class="bodytext">Thank you for supporting Partclone.</span><br />
+<span class="listwhitetext">
+<!--
+<br /><span class="bodytext"><strong>AUG, 2010</strong></span><br />
+<ul>
+<li>Tue Aug 20 - <a href="" target="_blank"></a></li>
+</ul>
+-->
+<br /><span class="bodytext"><strong>DEC, 2010</strong></span><br />
+<ul>
+<li>Fri Dec 24 - <a href="http://www.freevector.com/" target="_blank">Free Vectors</a></li>
+<li>Wed Dec 22 - <a href="http://www.tech-faq.com/" target="_blank">Tech-Faq</a></li>
+<li>Tue Dec 21 - <a href="http://www.juicerreviews.com/" target="_blank">Juicer Reviews</a></li>
+<li>Tue Dec 07 - <a href="http://www.compu-zone.de/" target="_blank">Download Usenext</a></li>
+</ul>
+
+<br /><span class="bodytext"><strong>NOV, 2010</strong></span><br />
+<ul>
+<li>Thu Nov 11 - <a href="http://www.kokot-finanzplanung.de" target="_blank">Honorarberatung durch Honorarberater</a></li>
+</ul>
+<br /><span class="bodytext"><strong>OCT, 2010</strong></span><br />
+<ul>
+<li>Sun Oct 31 - <a href="http://www.handy-bundle4u.de" target="_blank">Handy Bundle</a></li>
+<li>Tue Oct 26 - <a href="http://www.stop-smoking-tips.com/" target="_blank">How To Quit Smoking</a></li>
+<li>Sat Oct 23 - <a href="http://www.kostenlos-online-pokern.com/" target="_blank">Pokern</a></li>
+<li>Tue Oct 12 - <a href="http://www.travelprotect.de/" target="_blank">TravelProtect GmbH</a></li>
+<li>Sat Oct 09 - <a href="http://www.reiseversicherungsforum.de/" target="_blank">Reiseversicherungsforum</a></li>
+<li>Thu Oct 07 - <a href="http://www.uncut-dealer.com/" target="_blank">Uncut dvd shop</a></li>
+<li>Thu Oct 07 - <a href="http://www.hilfsmitteldiscount.de/" target="_blank">rollstuhl</a></li>
+</ul>
+
+<br /><span class="bodytext"><strong>SEP, 2010</strong></span><br />
+<ul>
+<li>Sun Sep 05 - <a href="http://www.umts-surf-stick.com/" target="_blank">Surfstick</a></li>
+</ul>
+
+<br /><span class="bodytext"><strong>AUG, 2010</strong></span><br />
+<ul>
+<li>Tue Aug 20 - <a href="http://www.hilfsmitteldiscount.de/bad-wc.html?limit=all" target="_blank">Badewannensitz </a></li>
+<li>Tue Aug 20 - <a href="http://www.hilfsmitteldiscount.de" target="_blank">Rollator </a></li>
+<li>Tue Aug 10 - <a href="http://www.autoteile-preisbrecher.de" target="_blank">Anhängerkupplungen</a></li>
+</ul>
+
+</span>
+</div>
+<?php include("footer.php");?>
