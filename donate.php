6bc446ddca5f3231324ec6cab14c2b8566ad63b3
Thomas Tsai <thomas@nchc.org.tw>
2011-04-26 10:22:35 +0800

    Donation

--- donate.php
+++ donate.php
@@ -27,6 +27,7 @@ Donate via PayPal: <input name="amount" value="5" type="radio"> $5 <input name="
 -->
 <br /><span class="bodytext"><strong>Apr, 2011</strong></span><br />
 <ul>
+<li>Tue Apr 26 - <a href="http://www.strandkorb-kaufen.net" target="_blank">Strandkorb kaufen</a></li>
 <li>Sat Apr 24 - <a href="http://www.massageliege.de" target="_blank">Massageliege</a></li>
 <li>Fri Apr 22 - <a href="http://www.seomatrix.de" target="_blank">Seo Matrix</a></li>
 <li>Thu Apr 21 - <a href="http://donkmag.net" target="_blank">Poker</a></li>
