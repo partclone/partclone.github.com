80b8a6c311cf95ea422881bb49bb848563f85523
Thomas Tsai <thomas@nchc.org.tw>
2011-06-30 09:03:30 +0800

    Donation

--- donate.php
+++ donate.php
@@ -27,6 +27,7 @@ Donate via PayPal: <input name="amount" value="5" type="radio"> $5 <input name="
 -->
 <br /><span class="bodytext"><strong>June, 2011</strong></span><br />
 <ul>
+<li> Thu June 30 - <a href="http://erstes-date.com" target="_blank">Erstes Date</a></li>
 <li> Thu June 30 - <a href="http://www.linkch.info" target="_blank">Webkatalog</a></li>
 <li> Thu June 30 - <a href="http://www.finest-silver-gallery.com" target="_blank">Silberschmuck</a></li>
 <li> Wed June 22 - <a href="http://designskins.com/de/" target="_blank">Designfolien</a></li>
