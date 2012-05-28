e21b488b75f2912fbacf089b7f3cf6fbbe188acc
Thomas Tsai <thomas@nchc.org.tw>
2012-05-28 23:13:39 +0800

    remove all

--- news.php
+++
@@ -1,7 +0,0 @@
-<?php include("header.php");?>
-
-<iframe src ="https://sourceforge.net/news/?group_id=183378" width="100%" height="400">
-  <p>Your browser does not support iframes.</p>
-</iframe>
-
-<?php include("footer.php");?>
