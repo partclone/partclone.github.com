54d9e213bf24be84121b6c1cdaa7913b0d4093a4
Thomas Tsai <thomas@nchc.org.tw>
2012-05-30 09:25:51 +0800

    update manual page

--- update_pcl_manpages.sh
+++ update_pcl_manpages.sh
@@ -19,11 +19,15 @@ manpages=$(find $partclone_docs -name "*.xml")
 pushd $partclone_usage_dir
 for man in $manpages; do
     $xsltproc $opt_css $stylesheet $man > $(basename $man).html
-    sed -i "s/<body.*/<!-- The start of edit area 2cce8a -->/" $(basename $man).html
+    sed -i "s/<body.*/<!-- The start of edit area 2cce8a -->\n<h3>Welcome to Partclone Usage<\/h3>\n<p>partclone.[ <a href=\"partclone.xml.html\">[fstype]<\/a> | <a href=\"partclone.restore.xml.html\">restore<\/a> | <a href=\"partclone.dd.xml.html\">dd<\/a> | <a href=\"partclone.chkimg.xml.html\">chkimg<\/a> | <a href=\"partclone.info.xml.html\">info<\/a> ]<\/p>/" $(basename $man).html
     sed -i "s/<\/body>/<\!-- The end of edit area 2cce8a -->/" $(basename $man).html
-    perl apply_template.pl $(basename $man).html
-    #mv index.html $(basename $man).html
-    #rm toc.hhc htmlhelp.hhp
 done
+#sed -i "s/<span class=\"refentrytitle\">\(partclone\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+sed -i "s/<span class=\"refentrytitle\">\(partclone.info\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+sed -i "s/<span class=\"refentrytitle\">\(partclone.chkimg\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+sed -i "s/<span class=\"refentrytitle\">\(partclone.dd\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+sed -i "s/<span class=\"refentrytitle\">\(partclone.restore\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+sed -i "s/<span class=\"refentrytitle\">\(partclone\)<\/span>/<span class=\"refentrytitle\"><a href=\"\1.xml.html\" alt=\"manual of \1\">\1<\/a><\/span>/g" *.xml.html
+find . -name "*.xml.html" -exec perl apply_template.pl '{}' \;
 popd
 echo "done"
